e6a77da5025689642a055d1b548d1e53421ff4b9
Jeremias <torres.bjeremias@gmail.com>
2024-01-13 22:14:06 -0300

    first commit

---
+++ public/js/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwfLwFpCY7oBZVLV278dkqvoxdtNv3pqA",
+  authDomain: "musica-somos.firebaseapp.com",
+  projectId: "musica-somos",
+  storageBucket: "musica-somos.appspot.com",
+  messagingSenderId: "196769541973",
+  appId: "1:196769541973:web:027b7dcae5ac558d8909dc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
